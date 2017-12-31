b10994a45f7cdd07dac2486de16db3fd66de273c
sozemego <piknypiec@gmail.com>
2017-12-31 22:24:08 +0100

    Hopefully better isInteger checking

--- utils/src/main/java/com/soze/kleddit/utils/CommonUtils.java
+++ utils/src/main/java/com/soze/kleddit/utils/CommonUtils.java
@@ -7,13 +7,15 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class CommonUtils {
 
+  private static final String ALPHANUMERIC_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
   public static String generateRandomString(int length) {
-    if(length < 0) {
+    if (length < 0) {
       throw new IllegalArgumentException("Length cannot be negative.");
     }
 
     StringBuilder sb = new StringBuilder(length);
-    for(int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++) {
       sb.append(generateRandomCharacterAlphanumeric());
     }
 
@@ -21,19 +23,18 @@ public class CommonUtils {
   }
 
   public static Character generateRandomCharacterAlphanumeric() {
-    String availableCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-
-    return availableCharacters.charAt(randomNumber(0, availableCharacters.length()));
+    return ALPHANUMERIC_CHARACTERS.charAt(randomNumber(0, ALPHANUMERIC_CHARACTERS.length()));
   }
 
   /**
    * Generates a random number between min and max. Min is inclusive, max is exclusive.
+   *
    * @param min min
    * @param max max
    * @return random number between min and max
    */
   public static int randomNumber(int min, int max) {
-    if(max < min) {
+    if (max < min) {
       throw new IllegalArgumentException("Max cannot be less than min.");
     }
 
@@ -41,11 +42,24 @@ public class CommonUtils {
   }
 
   public static int parseInt(String str, int defaultValue) {
-    try {
-      return Integer.parseInt(str);
-    } catch (NumberFormatException e) {
-      return defaultValue;
+    return isInteger(str) ? Integer.parseInt(str) : defaultValue;
+  }
+
+  /**
+   * Returns true if the given str represents an integer.
+   * False otherwise
+   *
+   * @param str input string
+   * @return true if given string can be parsed into an integer. False otherwise
+   */
+  public static boolean isInteger(String str) {
+    char[] chars = str.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      if (!Character.isDigit(chars[i])) {
+        return false;
+      }
     }
+    return true;
   }
 
 }
