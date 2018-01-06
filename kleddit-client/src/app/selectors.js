80a218ab694dc359f5e0798813954cd19c1d6c8a
sozemego <piknypiec@gmail.com>
2018-01-06 19:24:02 +0100

    Selector fix

--- kleddit-client/src/app/selectors.js
+++ kleddit-client/src/app/selectors.js
@@ -3,4 +3,4 @@ import { rootSelector } from '../state/utils';
 export const appRoot = rootSelector('app');
 
 export const isFetching = state => appRoot(state).fetchingActions > 0;
-export const getErrorMessage = state => appRoot(state).errorMessage;
+export const getErrorMessage = state => appRoot(state).errorMessage || '';
