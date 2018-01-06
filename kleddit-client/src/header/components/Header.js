e9e4a78a15e6711aeea45eee3e8e523f47032f6d
sozemego <piknypiec@gmail.com>
2018-01-06 20:15:09 +0100

    Removed css from header and also refactored styles to not have quotes

--- kleddit-client/src/header/components/Header.js
+++ kleddit-client/src/header/components/Header.js
@@ -2,28 +2,65 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { LinearProgress } from 'material-ui';
 
-import './header.css';
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderBottom: '1px solid white',
+  },
+  element: {
+    width: '33.3%',
+    display: 'flex',
+  },
+  elements: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: '15px 25px 15px 25px',
+    backgroundColor: 'rgba(15, 15, 15, 1)',
+    zIndex: '800',
+  },
+  progressFetching: {},
+  progressNotFetching: {
+    visibility: 'hidden',
+  },
+};
+
+styles.leftElement = Object.assign({}, styles.element, {
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+});
+
+styles.appName = Object.assign({}, styles.element, {
+  fontSize: '1.5rem',
+  justifyContent: 'center',
+});
+
+styles.rightElement = Object.assign({}, styles.element, {
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+});
 
 export const Header = (props) => {
   const {
     navigateToMain,
     isFetching,
     buttons,
-    leftButtons
+    leftButtons,
   } = props;
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', borderBottom: '1px solid white' }}>
-      <div className="header-container">
-        <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-start", width: "33.33%"}}>
+    <div style={styles.container}>
+      <div style={styles.elements}>
+        <div style={styles.leftElement}>
           {leftButtons}
         </div>
-        <div className="header-section header-app-name">
-          <div className="link" onClick={navigateToMain}>KLEDDIT</div>
+        <div style={styles.appName}>
+          <div onClick={navigateToMain}>KLEDDIT</div>
         </div>
-        <div className="header-section header-buttons-container">{buttons}</div>
+        <div style={styles.rightElement}>{buttons}</div>
       </div>
-      <LinearProgress mode="indeterminate" style={isFetching ? {} : { visibility: 'hidden' }}/>
+      <LinearProgress mode="indeterminate" style={isFetching ? styles.progressFetching : styles.progressNotFetching}/>
     </div>
   );
 };
@@ -36,5 +73,5 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
-  leftButtons: ""
+  leftButtons: '',
 };
