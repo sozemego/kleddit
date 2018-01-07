1b45488950621fe1181a8b06da37677097aeed7e
sozemego <piknypiec@gmail.com>
2018-01-07 18:57:39 +0100

    hashRouter

--- kleddit-client/src/kleddit/Kleddit.js
+++ kleddit-client/src/kleddit/Kleddit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import HeaderContainer from '../header/containers/HeaderContainer';
 import ErrorDisplay from '../app/containers/ErrorDisplay';
